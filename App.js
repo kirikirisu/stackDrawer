565bc7377fdb66d8548606aecb4026f46376f63a
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2019-03-19 22:30:34 +0900

    test2

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
-import Drawer from "./components/drawer";
+import drawer from "./components/drawer";
 
-const AppContainer = createAppContainer(Drawer);
+const AppContainer = createAppContainer(drawer);
 
 const App = () => (
   <AppContainer />
